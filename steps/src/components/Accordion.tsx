c888582b7646ef3572db8016b6f1453fadd22047
Hotimpulse <hotimpulse@hotmail.com>
2024-08-02 11:46:19 +0300

    feat: refining toggle functionality and closing on click, using the children prop

--- steps/src/components/Accordion.tsx
+++ steps/src/components/Accordion.tsx
@@ -1,36 +1,51 @@
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
+
+interface IAccordion {
+  key?: string;
+  num: number | null;
+  title: string;
+  text?: string;
+  curOpen: number | null;
+  onOpen: (arg: number | null) => void;
+  children?: ReactNode;
+}
 
 export default function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState<number | null>(null);
+
   return (
     <div className="accordion">
-      {data.map((item, index) => (
+      {data.map((item: IAccordion, index: number) => (
         <AccordionItem
           key={`${item.title}_${index}`}
           num={index}
           title={item.title}
-          text={item.text}
-        />
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+        >
+          {item.text}
+        </AccordionItem>
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }: IAccordion) {
+  const isOpen = num === curOpen;
 
   function handleToggle(): void {
-    setIsOpen((isOpen) => !isOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return (
     <>
       <div className="content-box" onClick={handleToggle}>
         <div className={`item ${isOpen ? "open" : ""}`}>
-          <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+          {num && <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>}
           <p className="title">{title}</p>
           <p className="icon">{isOpen ? `-` : `+`}</p>
         </div>
-        {isOpen && <p className="item">{text}</p>}
+        {isOpen && <div className="item">{children}</div>}
       </div>
     </>
   );
